f9e34759b08df83807795dfe472c8dd3d7050088
Cris Messias <meunomeecriss@gmail.com>
2022-02-25 18:39:31 -0300

    :tada: Begin a project.

---
+++ movieApp/movieApp/movieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieAppApp.swift
+//  movieApp
+//
+//  Created by Cris Messias on 25/02/22.
+//
+
+import SwiftUI
+
+@main
+struct movieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
